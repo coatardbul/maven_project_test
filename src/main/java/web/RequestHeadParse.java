f025af979ce6b6709b55b676509de8cc864150db
coatardbul <sxl14459048@gmail.com>
2018-10-30 11:26:39 +0800

    update_2018_10_30_11_26 add web project

---
+++ src/main/java/web/RequestHeadParse.java
@@ -0,0 +1,20 @@
+package web;
+
+public class RequestHeadParse {
+
+    public void RequestHeadParsePrint() {
+//        Map<String, String[]> paramMapTest = httpServletRequest.getParameterMap();
+//        Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
+//        //获取获取的消息头名称，获取对应的值，并输出
+//        while(headerNames.hasMoreElements()){
+//            String nextElement = headerNames.nextElement();
+//            System.out.println(nextElement+":"+httpServletRequest.getHeader(nextElement));
+//            if("Content-Type".equals(nextElement)&&httpServletRequest.getHeader(nextElement).contains("multipart/form-data") ){
+//                CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+//                MultipartHttpServletRequest multiReq = multipartResolver.resolveMultipart(httpServletRequest);
+//                //String post_csrftoken=multiReq.getParameter(Const.SESSION_CSRFTOKEN);//获取参数中的token
+//                httpServletRequest = multiReq;//将转化后的reuqest赋值到过滤链中的参数 *重要
+//            }
+//        }
+    }
+}
