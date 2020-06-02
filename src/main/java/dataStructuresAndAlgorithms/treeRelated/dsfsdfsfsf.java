77ad87fd9a93330ee0ad9bc1a7ff1506eb41c07b
coatardbul <36257640+coatardbul@users.noreply.github.com>
2020-06-02 13:56:21 +0800

    lock+leetcode

---
+++ src/main/java/dataStructuresAndAlgorithms/treeRelated/dsfsdfsfsf.java
@@ -0,0 +1,10 @@
+package dataStructuresAndAlgorithms.treeRelated;
+
+public class dsfsdfsfsf {
+    class BinaryNode{
+        Object element;
+        BinaryNode left;
+        BinaryNode right;
+    }
+}
+
